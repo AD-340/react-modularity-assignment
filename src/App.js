66ee495342d3d7ae26eb649a6bcf8916bd20cffe
NahomAlemu <nahom.worku2015@gmail.com>
2025-05-18 19:31:15 -0700

    remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { ContentA } from "./components/ContentA";
 import { ContentB } from "./components/ContentB";
-import { Button } from "./components/SharedComponents";
 
 function App() {
   return (
